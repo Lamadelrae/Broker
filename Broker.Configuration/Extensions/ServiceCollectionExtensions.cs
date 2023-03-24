89c23fd968a7211b2d1b19399e46399c8af465f4
Lamadelrae <matthew.almeida572@gmail.com>
2023-03-24 15:03:22 -0300

    adding fluent interface

--- Broker.Configuration/Extensions/ServiceCollectionExtensions.cs
+++ Broker.Configuration/Extensions/ServiceCollectionExtensions.cs
@@ -6,9 +6,10 @@ namespace Broker.Configuration.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static void AddBroker<TBroker>(this IServiceCollection services, Action<BrokerOptions<TBroker>> configure) where TBroker : class, IBroker<TBroker>
+    public static IServiceCollection AddBroker<TBroker>(this IServiceCollection services, Action<BrokerOptions<TBroker>> configure) where TBroker : class, IBroker<TBroker>
     {
         services.AddSingleton<IBroker<TBroker>, TBroker>();
         services.AddSingleton(BrokerOptions<TBroker>.FromAction(configure));
+        return services;
     }
 }
