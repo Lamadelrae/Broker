34f59a36a3c762a30d1841c0f8020714afb880fa
Lamadelrae <matthew.almeida572@gmail.com>
2023-03-23 23:31:14 -0300

    implementing command Handler

--- Broker.Consumer/Commands/UpdatePersonCommand.cs
+++ Broker.Consumer/Commands/UpdatePersonCommand.cs
@@ -6,3 +6,12 @@ public class UpdatePersonCommand : IRequest
 {
     public string? Name { get; set; }
 }
+
+
+public class UpdatePersonCommandHandler : IRequestHandler<UpdatePersonCommand>
+{
+    public Task Handle(UpdatePersonCommand request, CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
